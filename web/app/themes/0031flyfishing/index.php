ef47a3e65336cf72b386f57877889fb5935654e0
Vincent Derks <vincent.derks@webengine.nl>
2017-04-08 17:39:48 +0200

    Added apple web-app-capable meta tag

--- web/app/themes/0031flyfishing/index.php
+++ web/app/themes/0031flyfishing/index.php
@@ -4,6 +4,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
+<meta name="apple-mobile-web-app-capable" content="yes">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
